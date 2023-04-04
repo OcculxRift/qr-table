821294d32982d98f0a82757f55660d8127cdceec
OcculxRift <129623572+OcculxRift@users.noreply.github.com>
2023-04-05 00:53:57 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,37 @@
-$(document).ready(function(){
-  $.getJSON("https://occul.ticketers.club/status", function(data) {
-    var table_data = '';
-    var count = 1;
-    $.each(data, function(key, value) {
-      table_data += '<tr>';
-      table_data += '<td>' + count + '</td>';
-      table_data += '<td>' + value[1] + '</td>';
-      table_data += '<td>' + value[2] + '</td>';
-      table_data += '<td>' + new Date().toLocaleDateString() + '</td>';
-      table_data += '</tr>';
-      count++;
-    });
-    $('#occul_table').append(table_data);
-  });
-});
+async function fetchData() {
+  const response = await fetch('https://occul.ticketers.club/status');
+  const data = await response.json();
+  return data;
+}
+
+async function displayData() {
+  const data = await fetchData();
+
+  const tableBody = document.getElementById('table-body');
+  let rowNumber = 1;
+  for (const item of data) {
+    const row = document.createElement('tr');
+    const numberCell = document.createElement('td');
+    numberCell.textContent = rowNumber;
+    row.appendChild(numberCell);
+
+    const statusCell = document.createElement('td');
+    statusCell.textContent = item[0];
+    row.appendChild(statusCell);
+
+    const nameCell = document.createElement('td');
+    nameCell.textContent = item[1];
+    row.appendChild(nameCell);
+
+    const dateCell = document.createElement('td');
+    const date = new Date();
+    const options = { day: 'numeric', month: 'numeric', year: 'numeric' };
+    dateCell.textContent = date.toLocaleDateString('ru-RU', options);
+    row.appendChild(dateCell);
+
+    tableBody.appendChild(row);
+    rowNumber++;
+  }
+}
+
+displayData();
