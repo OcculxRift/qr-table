761501cbb41183ecf42d5e87e7ae87b58fa79acf
OcculxRift <129623572+OcculxRift@users.noreply.github.com>
2023-04-05 00:05:20 +0600

    Update index.php

--- index.php
+++ index.php
@@ -1,18 +1,31 @@
-<?php
-$url = 'https://occul.ticketers.club/status';
-$data = file_get_contents($url);
-$tableData = '';
-if (!empty($data)) {
-    $json = json_decode($data, true);
-    if (!empty($json)) {
-        foreach ($json as $row) {
-            $tableData .= '<tr>';
-            $tableData .= '<td>'.$row[0].'</td>';
-            $tableData .= '<td>'.$row[1].'</td>';
-            $tableData .= '<td>'.$row[2].'</td>';
-            $tableData .= '<td>'.$row[3].'</td>';
-            $tableData .= '</tr>';
-        }
-    }
-}
-?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Статусы</title>
+	<meta charset="utf-8">
+</head>
+<body>
+	<table>
+		<tr>
+			<th>№</th>
+			<th>Название</th>
+			<th>Статус</th>
+			<th>Дата</th>
+		</tr>
+		<?php
+		$url = 'https://occul.ticketers.club/status';
+		$json = file_get_contents($url);
+		$data = json_decode($json, true);
+
+		foreach ($data as $row) {
+			echo '<tr>';
+			echo '<td>'.$row[0].'</td>';
+			echo '<td>'.$row[1].'</td>';
+			echo '<td>'.$row[2].'</td>';
+			echo '<td>'.$row[3].'</td>';
+			echo '</tr>';
+		}
+		?>
+	</table>
+</body>
+</html>
